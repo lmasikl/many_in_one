980b62704da72cb644026260ecd8a93b284d5926
Maxim Krivodaev <maxim.krivodaev@kbor.ru>
2014-09-28 23:54:48 +0400

    Improve functional.

--- collect.py
+++ collect.py
@@ -1,34 +1,59 @@
-# -*- coding:utf-8 -*-
+# coding=utf-8
 """
-Collect many files in dir on one file.
+Collect many files in one.
 """
 import os
+import sys
+import re
 
 __author__ = 'maxim'
 __project__ = 'many_in_one'
 
+EXCLUDE_DIRS = ['.idea', '.git', 'migrations', 'vendor']
 
-def collect():
-    with open('output.md', 'w+') as output_file:
-        for _dir, dirs, files in os.walk('.'):
-            if './.idea' in _dir or './.git' in _dir:
+EXCLUDE_FILES = []
+
+FILE_TYPES = r'^.*\.(py|coffee)$'
+
+LOG = True
+
+
+def log(message):
+    if LOG:
+        print(message)
+
+
+def collect(_path):
+    with open('source_code.txt', 'w+') as output_file:
+        for _dir, dirs, files in os.walk(_path):
+            if True in map(lambda x: x in _dir, EXCLUDE_DIRS):
+                log('Dir {0} was excluded.'.format(_dir))
                 continue
 
-            for ignore in ['.gitignore', 'output.md', '__init__.py']:
-                if ignore in files:
-                    files.pop(files.index(ignore))
-
             for file_name in files:
-                with open(os.path.join(_dir, file_name)) as input_file:
+                if file_name in EXCLUDE_FILES or not re.match(FILE_TYPES, file_name):
+                    log('File {0} was excluded.'.format(file_name))
+                    continue
+
+                file_name = os.path.join(_dir, file_name)
+                with open(file_name) as input_file:
                     output_file.write('*{file}*\n\n'.format(file=file_name))
-                    # output_file.write('```python\n\n')
                     output_file.write(input_file.read())
-                    # output_file.write('\n\n```')
+                    output_file.write('\n\n')
+                    output_file.write('=' * 120)
                     output_file.write('\n\n')
 
-                print('File {0} was read.'.format(os.path.join(_dir, file_name)))
+                log('File {0} was read.'.format(file_name))
 
+
+def main():
+    if len(sys.argv) < 2:
+        _path = '.'
+    else:
+        _path = sys.argv[1]
+
+    collect(_path)
 
 
 if __name__ == '__main__':
-    collect()
+    main()
